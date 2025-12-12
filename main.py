5337439cd6fc16e08a6421a87b67bfcce903de78
jacoo444 <j5f5ystd5y@privaterelay.appleid.com>
2025-12-12 18:09:33 +0100

    added  a main (placeholder still)

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Code that manages everything
+"""
+import q_learning
+
+print("Your location: Barceloneta (41.12, 2.23)")
+print("Your destination: Sagrada Familia (41.05, 2.07)")
+
+# start = find closest street id to us in map_streets
+# goal = find closest street id to goal in map_streets
+# path = q_learning.train(start, goal, pathfind=True)
