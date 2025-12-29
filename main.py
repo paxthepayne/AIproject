9c46bfdc4405af6acc050f7d818a1ae0a95d3085
jacoo444 <j5f5ystd5y@privaterelay.appleid.com>
2025-12-29 14:20:41 +0100

    updated main

--- main.py
+++ main.py
@@ -33,18 +33,19 @@ if __name__ == "__main__":
 
     # Load Map Data as DataFrame
     with gzip.open("map.json.gz", "rt", encoding="utf-8") as f: data = json.load(f)
-    df = pd.DataFrame(data).set_index("id")
+    df = pd.DataFrame(data).set_index("id").rename(columns={"coords": "coordinates", "len": "length", "conns": "connections"})
 
-    city_map = df.rename(columns={"coords": "coordinates", "len": "length", "conns": "connections",})[["type", "name", "coordinates", "length", "connections"]]
-    
+    # Separate Q-learning map and popular times
+    city_map = df[["type", "name", "coordinates", "length", "connections"]]
     populartimes = df[["popular_times"]]
 
-    # Time and Weather (Sunny/Cloudy/Rainy)
+    # Get current Time and Weather (Sunny/Cloudy/Rainy)
     print("Fetching weather info...")
     current_time = datetime.datetime.now()
     weather = tools.get_weather()
     print(f"> {current_time.strftime('%A %H:%M')}, {weather}\n")
-    
+
+    # Get current crowd levels from populartimes, based on current time and weather
     weekday, hour = current_time.weekday(), current_time.hour
     weather_modifier = 1.3 if weather == "Sunny" else 0.3 if weather == "Rainy" else 0.9
     current_crowds = weather_modifier * populartimes["popular_times"].apply(lambda x: x[weekday][hour] if x is not None else 0.0)
@@ -62,7 +63,7 @@ if __name__ == "__main__":
                 (start_id, goal_id, city_map, current_crowds, True),  # shortest_path = True
             ],
         )
-    (path, Q), (shortest_path, _) = results
+    (path, q_table), (shortest_path, _) = results
 
     # Report Results
     path_length = city_map.loc[path, "length"].sum()
@@ -77,23 +78,16 @@ if __name__ == "__main__":
         for s in shortest_path
     )
 
-    path_names = city_map.loc[path, "name"].tolist()
-    shortest_path_names = city_map.loc[shortest_path, "name"].tolist()
-    clean_path = tools.clean(path_names)
-    clean_shortest_path = tools.clean(shortest_path_names)
+    clean_path = tools.clean(city_map.loc[path, "name"].tolist())
+    clean_shortest_path = tools.clean(city_map.loc[shortest_path, "name"].tolist())
 
     print(f"\n[Suggested path] {int(path_length)} meters | {path_total_crowd*1000/path_length:.2f} average crowd exposure")
     print(" -> ".join(clean_path), "\n")
     print(f"[Shortest path] {int(shortest_path_length)} meters | {shortest_path_total_crowd*1000/shortest_path_length:.2f} average crowd exposure")
     print(" -> ".join(clean_shortest_path), "\n")
 
+    # Optional Navigation Mode
     choice = input("Enter navigation mode? (y/n): ").strip().lower()
     if choice == "y":
-        tools.navigation_mode(
-            start_id,
-            goal_id,
-            city_map,
-            current_crowds,
-            Q
-        )
+        tools.navigation_mode(start_id, goal_id, city_map, current_crowds, q_table)
 
